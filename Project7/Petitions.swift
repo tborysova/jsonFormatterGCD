6cb681d5b339a2872e9d675c9d9149101c6783ba
tborysova <borysova.tetiana@gmail.com>
2022-10-13 18:58:49 +0200

    Added parsing JSON logic

---
+++ Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Tetiana Borysova on 13.10.22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+    
+}
